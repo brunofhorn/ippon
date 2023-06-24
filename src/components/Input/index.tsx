0234153898ec2f06e309549c73020b97d2763461
brunofhorn <b.fernandeshorn@gmail.com>
2023-06-24 04:41:19 -0300

    feat: input css

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -40,7 +40,7 @@ export const Input = forwardRef<HTMLInputElement, IInput>(
                     </div>
                 )}
                 <input
-                    className="peer border border-gray_two block min-h-[auto] focus:border-secondary w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="peer border border-gray_two block min-h-[auto] focus:ring-secondary w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     ref={ref}
                     placeholder=" "
                     id={id}
@@ -53,7 +53,11 @@ export const Input = forwardRef<HTMLInputElement, IInput>(
                     pattern={getMask(mask)}
                     {...rest}
                 />
-                <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:bg-white peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
+                <label
+                    className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:bg-white peer-focus:-translate-y-[1.15rem] ${
+                        value ? "-translate-y-[1.15rem]" : ""
+                    } peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary`}
+                >
                     {label}
                 </label>
             </div>
